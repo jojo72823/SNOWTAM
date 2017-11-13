d82261bd29fcb6e4a3a72d1e2c61c16a9ae1853f
jonathan daumont <jojo-d72@hotmail.fr>
2017-11-13 10:46:59 +0100

    commit init

---
+++ app/src/main/java/daumont/caspar/ensim/snowtam/ActivityStart.java
@@ -0,0 +1,13 @@
+package daumont.caspar.ensim.snowtam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
