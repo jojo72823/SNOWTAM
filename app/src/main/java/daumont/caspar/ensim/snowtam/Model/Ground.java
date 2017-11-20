82b014e7207163e9a950c31826bf9bf8b6c4b68d
thomas.caspar <thomas.caspar.etu@univ-lemans.fr>
2017-11-20 12:19:49 +0100

    Raw traitement

--- app/src/main/java/daumont/caspar/ensim/snowtam/Model/Ground.java
+++ app/src/main/java/daumont/caspar/ensim/snowtam/Model/Ground.java
@@ -9,6 +9,10 @@ public class Ground {
     private String name;
     private String data_crypt;
 
+    private String snowtam_raw ;
+
+    private String snowtam_decoded ;
+
     public Ground(String name){
         this.name = name;
     }
@@ -21,11 +25,28 @@ public class Ground {
         this.name = name;
     }
 
+    public String getSnowtam_raw() {
+        return snowtam_raw;
+    }
+
+    public void setSnowtam_raw(String snowtam_raw) {
+        this.snowtam_raw = snowtam_raw;
+    }
+
+    public String getSnowtam_decoded() {
+        return snowtam_decoded;
+    }
+
+    public void setSnowtam_decoded(String snowtam_decoded) {
+        this.snowtam_decoded = snowtam_decoded;
+    }
+
     public String getData_crypt() {
         return data_crypt;
     }
 
     public void setData_crypt(String data_crypt) {
         this.data_crypt = data_crypt;
+
     }
 }
